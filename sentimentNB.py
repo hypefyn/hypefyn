ea04c924c6409e5dc2869f0880c87bd11f711fe7
PietroDomiGT <pietrodomi@gatech.edu>
2020-04-09 11:07:44 +0200

    adjustment

--- sentimentNB.py
+++ sentimentNB.py
@@ -1,8 +1,5 @@
 # %%
 import json
-from sklearn.pipeline import Pipeline
-from sklearn.feature_extraction.text import HashingVectorizer, TfidfTransformer
-from sklearn.naive_bayes import MultinomialNB
 
 # %%
 with open("data/sentiment140/train_clean_joint.json",'r') as t:
@@ -11,8 +8,13 @@ with open("data/sentiment140/val_clean_joint.json",'r') as v:
     test = json.load(v)
 
 # %%
-pipeline = Pipeline([
-    ('vectorizer',HashingVectorizer(n_features=2**16)),  # strings to token integer counts
-    ('tfidf', TfidfTransformer()),  # integer counts to weighted TF-IDF scores
-    ('classifier', MultinomialNB()),  # train on TF-IDF vectors w/ Naive Bayes classifier
-])
+
+k = str(len(train))
+
+for d in test.values():
+    train[k] = d
+    k = str(int(k)+1)
+
+# %%
+with open("data/sentiment140/sentiment140_clean_joint.json",'w') as v:
+    json.dump(train,v)
